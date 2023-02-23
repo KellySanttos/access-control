b9917871509d7cfdcbd2c9ea9eed4ed566607a41
Kelly Santtos <kellytecnit@gmail.com>
2023-02-23 19:12:23 -0300

    Scanner adicionado

--- src/AccessControl.java
+++ src/AccessControl.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class AccessControl {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
     }
     public static int Multiply(int numberOne, int numberTwo) {
