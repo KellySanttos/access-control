a082e9f47a089c00d574f66e3abfc8790e2a5cde
Kelly Santtos <kellytecnit@gmail.com>
2023-02-23 19:10:50 -0300

    first commit

---
+++ src/AccessControl.java
@@ -0,0 +1,8 @@
+public class AccessControl {
+    public static void main(String[] args) {
+
+    }
+    public static int Multiply(int numberOne, int numberTwo) {
+        return numberOne * numberTwo;
+    }
+}
